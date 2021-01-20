7d44bebd438a1e3233f14cce20b46ee14594a4e9
Marcelo Silva <marcelo.silva@per.gov.ie>
2021-01-20 18:08:36 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HomePage from './homepage.component'
 import './App.css';
 
 function App() {
   return (
-    <div>Hello world!
+    <div>
+      <HomePage />
 
     </div>
   );
