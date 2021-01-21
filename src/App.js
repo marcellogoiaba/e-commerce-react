e2fe30cedea42454e82f9c0b7180c05426d2a42d
Marcelo Silva <marcelo.silva@per.gov.ie>
2021-01-21 17:33:24 +0000

    added custom inputs and custom buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signIn' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
