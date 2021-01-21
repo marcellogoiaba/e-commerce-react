e4c3d9e7c14c18658195c14f75aacf2dc4fe3ee2
Marcelo Silva <marcelo.silva@per.gov.ie>
2021-01-21 16:07:51 +0000

    added collections to the project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
